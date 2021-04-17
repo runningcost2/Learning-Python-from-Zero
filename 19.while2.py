67e02158d7b3c94e69bbee32a77ece90d1a20926
Kiwoong An <runningcost2@gmail.com>
2021-04-18 01:00:36 +0900

    Update 19.while2.py

--- 19.while2.py
+++ 19.while2.py
@@ -1,10 +1,14 @@
-# customer = "Robert"
+customer = "Robert"
 
-# index = 1 #Index to check how many times did they called customer
-# while True:
-#     print("{0}, Your order is ready.".format(customer))
-#     index += 1
+index = 1 #Index to check how many times did they called customer
+while True:
+    print("{0}, Your order is ready.".format(customer))
+    index += 1
 
-#     if index => 2
-#     print("{0}, Your order is ready. We called you {1} times".format(customer, index))
-#     index += 1
+    if index >= 3:
+        print("We called you {0} times".format(index - 1))
+        if index >= 15:
+            print("This is final call. Your order will dispose soon.")
+            if index >= 16:
+                print("Your order has been disposed")
+                break
